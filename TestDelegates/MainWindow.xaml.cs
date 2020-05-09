da215a546c6d02806277a4a7016ebe08017a069f
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-05-09 02:59:53 +0200

    Add project files.

---
+++ TestDelegates/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+
+namespace TestDelegates
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public delegate void TestDelegate(string testo);
+
+    public partial class MainWindow : Window
+    {
+        private TestDelegate testDelegate;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            testDelegate = new TestDelegate(OnTestDelegate1);
+            testDelegate += OnTestDelegate2;
+        }
+
+        private void OnTestDelegate1(string messaggio)
+        {
+            MessageBox.Show(messaggio);
+        }
+
+        private void OnTestDelegate2(string messaggio)
+        {
+            if (int.TryParse(messaggio, out int quanto))
+            {
+                MessageBox.Show((quanto + 1).ToString());
+            }
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            testDelegate(txt.Text);
+        }
+
+
+    }
+}
